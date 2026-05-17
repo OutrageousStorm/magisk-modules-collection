16d16336c771a9d4abed4787e8aee80773d43c08
Tom | Android Intelligence <gostorm426@gmail.com>
2026-05-17 06:25:50 -0400

    template: post-fs-data.sh

---
+++ module_template/post-fs-data.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+# Called after mounting the file system (early stage)
+# Best place for modifying system files
+
+# Example: replace a file
+# cp $MODDIR/system/app/MyApp.apk /system/app/MyApp/MyApp.apk
+
+true
